6686fc13897ec7d1c56628144c9887b617af7a79
yyfrankyy <yyfrankyy@gmail.com>
2011-11-13 22:26:27 +0800

    fix again

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+var n2a = require('./native2ascii').native2ascii;
 var a2n = require('./native2ascii').ascii2native;
 var assert = require('assert').equal;
 assert('y', a2n('y'));
+assert('中', a2n('\\u4e2d'));
